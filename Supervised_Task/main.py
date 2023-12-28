129a483fed1b76f7b0486afdfa9a01247fede98c
Ananya Kapoor <akapoor@uoregon.edu>
2023-12-28 08:43:40 -0800

    Fixing bug in the loss function.

--- Supervised_Task/main.py
+++ Supervised_Task/main.py
@@ -24,6 +24,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import umap
 import matplotlib.pyplot as plt
 import torch.optim as optim
+import itertools
 
 plt.rcParams.update({'font.size': 20})
 plt.rcParams['figure.figsize'] = [15, 15]  # width and height should be in inches, e.g., [10, 6]
@@ -58,7 +59,7 @@ with open(f'{filepath}/Dropbox (University of Oregon)/Kapoor_Ananya/01_Projects/
 
 # The below cell initializes an object that will store useful information for contrastive learning
 
-# In[23]:
+# In[23]: Creating Dataset
 
 
 simple_tweetyclr_experiment_1 = Tweetyclr(num_spec, window_size, stride, folder_name, all_songs_data, masking_freq_tuple, spec_dim_tuple, category_colors)
@@ -67,40 +68,83 @@ simple_tweetyclr = simple_tweetyclr_experiment_1
 simple_tweetyclr.first_time_analysis()
 
 
-embed = np.load(f'{simple_tweetyclr.folder_name}/embed_of_labels_hamming.npy')
+# In[24]: Raw UMAP using Hamming Distance
+# The locations where the syllable is present should have a value of 1 and the
+# locations that are silence should have a value of 0. This UMAP decomposition
+# is NOT using power values 
 
-# plt.figure()
-# plt.scatter(embed[:,0], embed[:,1], c = simple_tweetyclr.mean_colors_per_minispec)
-# plt.suptitle("UMAP Decomposition of Spectrogram Slice Syllable Labels")
-# plt.title(f'Number of Slices: {embed.shape[0]}')
-# # plt.savefig(f'{simple_tweetyclr.folder_name}/Plots/umap_decomp_of_spec_slice_labels.png')
-# plt.show()
+# spec_copy = simple_tweetyclr.stacked_specs.copy()
+# idx = np.where(simple_tweetyclr.stacked_labels == 0)[0]
+# spec_copy[:,idx] = 0
+# spec_copy[spec_copy!=0] = 1
+# spec_copy = spec_copy.T
+
+# # Let's calculate what dx is
+# dat = np.load('/Users/AnanyaKapoor/Dropbox (University of Oregon)/Kapoor_Ananya/01_Projects/01_a_Rotations/Gardner_Lab/Canary_Data/llb16_data_matrices/Python_Files/llb16_1775_2018_05_11_07_06_29.wav.npz')
+# dx = dat['t'][0,1] - dat['t'][0,0]
+
+# stacked_windows = []
+# for i in range(0, spec_copy.shape[0] - window_size + 1, stride):
+#     # Find the window
+#     window = spec_copy[i:i + window_size, :]
+#     # Get the window onset and ending times
+#     # We will flatten the window to be a 1D vector
+#     window = window.reshape(1, window.shape[0]*window.shape[1])
+#     # Extract the syllable labels for the window
+#     # Reshape the syllable labels for the window into a 1D array
+#     # Populate the empty lists defined above
+#     stacked_windows.append(window)
+
+# # Convert the populated lists into a stacked numpy array
+# stacked_windows = np.stack(stacked_windows, axis = 0)
+# stacked_windows = np.squeeze(stacked_windows)
+
+# np.save('/Users/AnanyaKapoor/Downloads/stacked_windows_hamming.npy', stacked_windows)
+# np.save('/Users/AnanyaKapoor/Downloads/mean_cols_arr.npy', simple_tweetyclr.mean_colors_per_minispec)
+
+
+
+embed = np.load(f'{simple_tweetyclr.folder_name}/embed_of_stacked_windows_hamming.npy')
+
+plt.figure()
+plt.scatter(embed[:,0], embed[:,1], c = simple_tweetyclr.mean_colors_per_minispec)
+plt.suptitle("UMAP Decomposition of Spectrograms Using Hamming Distance Similarity")
+plt.title(f'Number of Slices: {embed.shape[0]}')
+plt.savefig(f'{simple_tweetyclr.folder_name}/Plots/umap_decomp_of_spec_slice_hamming.png')
+plt.show()
 
 
 total_dataset = TensorDataset(torch.tensor(simple_tweetyclr.stacked_windows.reshape(simple_tweetyclr.stacked_windows.shape[0], 1, 100, 151)))
 batch_size = 64
 total_dataloader = DataLoader(total_dataset, batch_size=batch_size , shuffle=False)
 
+list_of_images = []
+for batch_idx, (data) in enumerate(total_dataloader):
+    data = data[0]
 
-# Let's create the images necessary for the Bokeh visualization
+    for image in data:
+        list_of_images.append(image)
 
-# In[13]:
+list_of_images = [tensor.numpy() for tensor in list_of_images]
+
+embeddable_images = simple_tweetyclr.get_images(list_of_images)
+
+simple_tweetyclr.plot_UMAP_embedding(embed, simple_tweetyclr.mean_colors_per_minispec, embeddable_images, f'{simple_tweetyclr.folder_name}/Plots/UMAP_of_window_labels_hamming.html', saveflag = True)
 
 
-# list_of_images = []
-# for batch_idx, (data) in enumerate(total_dataloader):
-#     data = data[0]
-
-#     for image in data:
-#         list_of_images.append(image)
-
-# list_of_images = [tensor.numpy() for tensor in list_of_images]
-
-# embeddable_images = simple_tweetyclr.get_images(list_of_images)
-
-# simple_tweetyclr.plot_UMAP_embedding(embed, simple_tweetyclr.mean_colors_per_minispec, embeddable_images, f'{simple_tweetyclr.folder_name}/Plots/UMAP_of_window_labels_hamming.html', saveflag = True)
 
 # In[14]: Let's create a siamese network to predict the hamming distance
+
+# I need to modify this architecture so that I am doing the following
+# 1. Batch size is user specified
+# 2. Loss is calculated as the pairwise absolute distance between observations in the batch 
+
+# This approach is a bit different than having a batch size of 2 and directly
+# predicting the hamming distance. I tried this approach in order to use 
+# computational power a bit better 
+
+# Question: is a model that is trained to match the batch pairwise hamming 
+# distance conducive to good phrase representations? 
 
 import torch
 import torch.nn as nn
@@ -137,10 +181,9 @@ class Encoder(nn.Module):
         self.fc = nn.Linear(320, 1)
 
         self._to_linear = 1
-        self.final_layer = nn.Linear(1 * 2, 1) 
         
 
-    def forward_method(self, x):
+    def forward(self, x):
 
         # x = F.relu((self.conv1(x)))
         # x = F.relu((self.conv2(x)))
@@ -165,27 +208,32 @@ class Encoder(nn.Module):
         x = F.relu(self.bn9(self.conv9(x)))
         x = F.relu(self.bn10(self.conv10(x)))
 
-        # x = x.view(-1, 320)
-        x_flattened = x.view(1, -1)
+        x_flattened = x.view(-1, 320)
+        # x_flattened = x.view(1, -1)
         # x = x.permute(0, x.shape[1]*x.shape[2]*x.shape[3])
         x = F.relu(self.fc(x_flattened))
+        
+        pairwise_differences = [torch.abs(obs2 - obs1) for obs1, obs2 in itertools.combinations(x, 2)]
+        pairwise_differences = torch.cat(pairwise_differences, dim = 0)
+        
+        hamming_like_distance = torch.sum(pairwise_differences)
         
         # hamming_like_distance = torch.sum(torch.abs(x[0,:] - x[1,:]))
 
 
         # return hamming_like_distance
         
-        return x
+        return hamming_like_distance
     
-    def forward(self, input1, input2):
-        zi = self.forward_method(input1)
-        zj = self.forward_method(input2)
+    # def forward(self, input1, input2):
+    #     zi = self.forward_method(input1)
+    #     zj = self.forward_method(input2)
         
-        dist = zi - zj
-        dist = torch.abs(dist)  # Element-wise absolute value
-        hamming_dist = dist
+    #     dist = zi - zj
+    #     dist = torch.abs(dist)  # Element-wise absolute value
+    #     hamming_dist = dist
         
-        return hamming_dist
+    #     return hamming_dist
         
 
 simple_tweetyclr.shuffling(295)
@@ -198,6 +246,8 @@ dataset = torch.tensor(simple_tweetyclr.stacked_windows.reshape(simple_tweetyclr
 
 dataset = TensorDataset(dataset, torch.tensor(simple_tweetyclr.stacked_labels_for_window))
 
+total_dataloader = DataLoader(dataset, batch_size=batch_size , shuffle=False)
+
 
 # Define the split sizes
 train_size = int(0.8 * len(dataset))  # 80% for training
@@ -208,8 +258,8 @@ train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 from torch.utils.data import DataLoader
 
-train_loader = DataLoader(train_dataset, batch_size=2, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 model = Encoder()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
@@ -217,7 +267,7 @@ criterion = nn.MSELoss()  # MSE Loss
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
-accumulation_steps = 32
+accumulation_steps = 1
 num_epochs = 500
 
 epoch_loss = []
@@ -229,14 +279,14 @@ for epoch in range(num_epochs):
         inputs = inputs.to(device, dtype=torch.float32)
         labels = labels.to(device, dtype=torch.float32)
         
-        input1, input2 = inputs[0,:,:,:], inputs[1,:,:,:]
-        input1 = input1.unsqueeze(0)
-        input2 = input2.unsqueeze(0)
+        pred_hamming = model(inputs)
         
-        pred_hamming = model(input1, input2)
+        actual_hamming = [(obs2 != obs1).sum().unsqueeze(0) for obs1, obs2 in itertools.combinations(labels, 2)]
+        actual_hamming = torch.cat(actual_hamming, dim = 0)
+        actual_hamming = torch.sum(actual_hamming).to(torch.float32)
         
-        actual_hamming = (labels[0,:] != labels[1,:]).sum().to(torch.float32)
-        actual_hamming = actual_hamming.view(1, 1)
+        # actual_hamming = (labels[0,:] != labels[1,:]).sum().to(torch.float32)
+        # actual_hamming = actual_hamming.view(1, 1)
         loss = criterion(pred_hamming, actual_hamming)
 
         # Scales the loss, and calls backward() to accumulate gradients
@@ -249,9 +299,11 @@ for epoch in range(num_epochs):
             optimizer.zero_grad()
 
         batch_loss += loss.item()
+        
 
     # Logging the loss averaged over an epoch
-    epoch_loss.append(batch_loss / len(train_loader))
+    epoch_loss.append(batch_loss)
+    # epoch_loss.append(batch_loss / len(train_loader))
 
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss[-1]}')
     
@@ -260,7 +312,30 @@ plt.figure()
 plt.plot(epoch_loss)
 plt.show()
 
+model_rep = []
 
+model.eval()
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(total_dataloader):
+        inputs = inputs.to(device, dtype=torch.float32)
+        labels = labels.to(device, dtype=torch.float32)
+    
+        x = F.relu(model.bn1(model.conv1(inputs)))
+        x = F.relu(model.bn2(model.conv2(x)))
+        x = F.relu(model.bn3(model.conv3(x)))
+        x = F.relu(model.bn4(model.conv4(x)))
+        x = F.relu(model.bn5(model.conv5(x)))
+        x = F.relu(model.bn6(model.conv6(x)))
+        x = F.relu(model.bn7(model.conv7(x)))
+        x = F.relu(model.bn8(model.conv8(x)))
+        x = F.relu(model.bn9(model.conv9(x)))
+        x = F.relu(model.bn10(model.conv10(x)))
+
+        x_flattened = x.view(-1, 320)
+        model_rep.append(x_flattened.clone().detach().numpy())
+        
+model_rep1 = np.concatenate((model_rep))    
+        
 
 
 
